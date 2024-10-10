6ab1539bb8e002315518d1da68846731a5cfd920
LEEYEONSEONG <dustjd1535@gmail.com>
2024-10-10 16:01:43 +0900

    Add : jest.config.ts 초기 환경설정

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest", // ts-jest를 사용하여 TypeScript 파일을 변환
+  testEnvironment: "jest-environment-jsdom", // react 컴포넌트 테스트를 위한 jsdom 환경 설정
+};
+
+export default config;
