0dc530c7f6817dbfb903164133090ad3629a8826
LEEYEONSEONG <dustjd1535@gmail.com>
2024-10-14 16:53:32 +0900

    Add : jest setup 설정 파일 추가

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "jest";
 const config: Config = {
   preset: "ts-jest", // ts-jest를 사용하여 TypeScript 파일을 변환
   testEnvironment: "jest-environment-jsdom", // react 컴포넌트 테스트를 위한 jsdom 환경 설정
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
 
 export default config;
