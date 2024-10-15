c7e6710fcaa723e154b34c61769177317413adc7
LEEYEONSEONG <dustjd1535@gmail.com>
2024-10-15 14:20:58 +0900

    Fix : jest config compiler option 설정 변경

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,17 @@
 import type { Config } from "jest";
 
 const config: Config = {
-  preset: "ts-jest", // ts-jest를 사용하여 TypeScript 파일을 변환
-  testEnvironment: "jest-environment-jsdom", // react 컴포넌트 테스트를 위한 jsdom 환경 설정
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/tsconfig.jest.json", // tsconfig 파일을 여기서 지정
+      },
+    ],
+  },
+
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
 
